e9bee2b8453f2f2a0b4d7f9b6d0f0e9bc6799fa4
EnriqueHdzSv <90428764+EnriqueHdzSv@users.noreply.github.com>
2022-09-18 18:54:41 -0500

    Mi primera aplicacion en JS

---
+++ app.js
@@ -0,0 +1,21 @@
+function despedir(){
+    alert("Adios desde archivo externo");
+}
+function aceptar(){
+    alert("Acepta cookies");
+}
+function advertencia(){
+    alert("Sitio de paga");
+}
+function sugerencia(){
+    alert("Para este sitio desactiva adblock");
+}
+console.log("Hola desde la consola");
+console.log("Este es un ...");
+console.log(20);
+console.log(2.6);
+console.log(1,63);
+console.log(1+2);
+console.log(true);
+console.log(false);
+
